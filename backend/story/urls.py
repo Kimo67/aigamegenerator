37a0751cd04ec6f786ac72ada1b50dc798de4e74
Divino Schaeffer <divino.schaeffer02@gmail.com>
2025-04-01 17:37:23 +0200

    🧱 Fixed docker

--- backend/story/urls.py
+++ backend/story/urls.py
@@ -5,7 +5,7 @@ from story.views.case import CaseViewSet
 
 router = DefaultRouter()
 router.register(r'characters', CharacterViewSet)  # This maps to /api/characters/
-router.register(r'case', CaseViewSet)  # This maps to /api/characters/
+router.register(r'case', CaseViewSet)  # This maps to /api/case/
 
 urlpatterns = [
     path('api/', include(router.urls)),
