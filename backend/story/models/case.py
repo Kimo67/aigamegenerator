02974d7edbe796f7e0e48df0d9385ebd76e3b4f5
Bryeinstein <bryan.dam05@gmail.com>
2025-04-12 12:04:06 +0200

    Feat: new table migrated

--- backend/story/models/case.py
+++ backend/story/models/case.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Case(models.Model):
     title = models.CharField(max_length=255)
-    characters = models.ManyToManyField('Character', through='CharacterCase', related_name='cases')
+    characters = models.ManyToManyField('Character', through='story.CharacterCase', related_name='cases')
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
