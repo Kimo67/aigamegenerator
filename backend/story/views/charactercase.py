ef67dfe5e7ef0a7de191489650ec554cbfbac2d8
Bryeinstein <bryan.dam05@gmail.com>
2025-04-13 10:52:01 +0200

    Fix: a character should have a unique link with a case - migrations

--- backend/story/views/charactercase.py
+++ backend/story/views/charactercase.py
@@ -1,3 +1,5 @@
+from django.db import IntegrityError
+from django.forms import ValidationError
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from ..models import CharacterCase
@@ -6,3 +8,9 @@ from ..serializers.charactercase import CharacterCaseSerializer
 class CharacterCaseViewSet(viewsets.ModelViewSet):
     queryset = CharacterCase.objects.all()
     serializer_class = CharacterCaseSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            raise ValidationError("This character is already linked to this case.")
