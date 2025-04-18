e0a8137acd7da08e53c2ae416c5ec4dfc195aac3
bendriss <mohamed.dris.bendriss@gmail.com>
2025-04-18 17:02:07 +0200

    Added parent field

--- backend/story/models/case.py
+++ backend/story/models/case.py
@@ -4,6 +4,9 @@ class Case(models.Model):
     title = models.CharField(max_length=255)
     characters = models.ManyToManyField('Character', through='story.CharacterCase', related_name='cases')
     description = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
+    background = models.TextField(blank=True, null=True)
+    parent = models.IntegerField(blank=True, null=False, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
