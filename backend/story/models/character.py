efd813115034e657b7fe993d55128985571b422d
bendriss <mohamed.dris.bendriss@gmail.com>
2025-05-10 13:22:37 +0200

    Migration bdd pour ajouter a un personnage une couleur et une image associé

--- backend/story/models/character.py
+++ backend/story/models/character.py
@@ -4,6 +4,8 @@ class Character(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
+    hexcolor = models.CharField(max_length=32,blank=True, null=True)
+    imagepath = models.CharField(max_length=512, blank=True, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
