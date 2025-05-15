97358236a24d7a63df51517e22874efae3cb6233
Mohamed Dris <mohamed-dris.bendriss@unistra.fr>
2025-05-15 18:59:11 +0200

    Ajout endpoint renpy depuis localhost:8001

--- backend/story/views/story.py
+++ backend/story/views/story.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets
 from ..models.story import Story
 from ..serializers.story import StorySerializer
+import requests
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 class StoryViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all()
@@ -15,3 +20,11 @@ class StoryViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Story.objects.filter(date_deleted__isnull=True)
+    
+    @action(detail=False, methods=['get'], url_path='renpy')
+    def custom_get(self, request):
+        # Custom logic here (e.g. filtered queryset, stats, etc.)
+        ai_response = requests.post(
+            "http://ai-script-app:8050/story/add-node",
+        )
+        return Response(ai_response)
